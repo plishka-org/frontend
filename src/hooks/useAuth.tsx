ff5d8a5c03f21ec5aaab00320afae53bbd2eedbd
DenysPavlish <denyspavlish23@lfkhpp.ukr.education>
2026-05-27 22:54:25 +0300

    fix useAuth

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,48 +1,53 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, useContext, useEffect, useState } from 'react'
-import type { ReactNode } from 'react'
-import { checkAuthStatus } from '../services/api/authApi'
+import { createContext, useContext, useEffect, useState } from "react";
+import type { ReactNode } from "react";
+import { checkAuthStatus } from "../services/api/authApi";
 
 interface User {
-  id: number
-  name: string
-  email: string
+  id: number;
+  name: string;
+  email: string;
 }
 
 interface AuthContextType {
-  user: User | null
-  isAuthChecked: boolean
-  login: (userData: User) => void
-  logout: () => void
+  user: User | null;
+  isAuthChecked: boolean;
+  login: (userData: User) => void;
+  logout: () => void;
 }
 
-const AuthContext = createContext<AuthContextType | null>(null)
+const AuthContext = createContext<AuthContextType | null>(null);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null)
-  const [isAuthChecked, setIsAuthChecked] = useState(false)
+  //коли буде бек замінити
+  const [user, setUser] = useState<User | null>({
+    id: 1,
+    name: "Тест",
+    email: "test@test.com",
+  });
+  const [isAuthChecked, setIsAuthChecked] = useState(false);
 
   useEffect(() => {
     checkAuthStatus()
       .then(({ isAuthenticated, user: userData }) => {
-        setUser(isAuthenticated && userData ? userData : null)
+        setUser(isAuthenticated && userData ? userData : null);
       })
       .catch(() => setUser(null))
-      .finally(() => setIsAuthChecked(true))
-  }, [])
+      .finally(() => setIsAuthChecked(true));
+  }, []);
 
-  const login = (userData: User) => setUser(userData)
-  const logout = () => setUser(null)
+  const login = (userData: User) => setUser(userData);
+  const logout = () => setUser(null);
 
   return (
     <AuthContext.Provider value={{ user, isAuthChecked, login, logout }}>
       {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
 export const useAuth = (): AuthContextType => {
-  const context = useContext(AuthContext)
-  if (!context) throw new Error('useAuth must be used within AuthProvider')
-  return context
-}
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("useAuth must be used within AuthProvider");
+  return context;
+};
